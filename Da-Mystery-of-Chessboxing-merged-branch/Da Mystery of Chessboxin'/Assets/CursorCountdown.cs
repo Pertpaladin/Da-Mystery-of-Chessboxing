9be1d7e25e9ccdd91beb6d67fc9d2b01c707f975
ged2642 <45339932+ged2642@users.noreply.github.com>
2019-04-15 20:33:37 -0500

    15AprilUpdate3

---
+++ Da-Mystery-of-Chessboxing-merged-branch/Da Mystery of Chessboxin'/Assets/CursorCountdown.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CursorCountdown : MonoBehaviour {
+    Text text;
+    int range;
+	// Use this for initialization
+	void Start () {
+        text = GetComponent<Text>();
+        range = GameObject.Find("SceneManager").GetComponent<GridSceneManager>().range;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        range = GameObject.Find("SceneManager").GetComponent<GridSceneManager>().range;
+
+        if (range > 0)
+        {
+            text.text = (GameManager.fighters[GameManager.turn].range - range).ToString(); 
+        } else
+        {
+            text.text = "";
+        }
+	}
+}
---
+++ Da-Mystery-of-Chessboxing-merged-branch/Da Mystery of Chessboxin'/Assets/Scripts/Song_selector.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Song_selector : MonoBehaviour {
+    public AudioSource audioManager;
+    public AudioClip first;
+    public AudioClip second;
+    public AudioClip third;
+    public AudioClip fourth;
+    private bool canPlay = true;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (canPlay)
+        {
+            switch (Random.Range(1, 4))
+            {
+                case 1:
+                    audioManager.clip = first;
+                    audioManager.Play();
+                    canPlay = false;
+                    StartCoroutine("PlayMoosic");
+                    break;
+                case 2:
+                    audioManager.clip = second;
+                    audioManager.Play();
+                    canPlay = false;
+                    StartCoroutine("PlayMoosic");
+                    break;
+                case 3:
+                    audioManager.clip = third;
+                    audioManager.Play();
+                    canPlay = false;
+                    StartCoroutine("PlayMoosic");
+                    break;
+                case 4:
+                    audioManager.clip = fourth;
+                    audioManager.Play();
+                    canPlay = false;
+                    StartCoroutine("PlayMoosic");
+                    break;
+                default:
+                    break;
+            }
+        }
+	}
+
+    IEnumerator PlayMoosic()
+    {
+        yield return new WaitForSecondsRealtime(360);
+        canPlay = true;
+    }
+
+}
